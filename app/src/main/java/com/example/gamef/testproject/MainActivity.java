92feadda06918c054c64c6031f3982962fe02f6d
bunny1997 <priyank20patel@gmail.com>
2017-02-10 15:33:53 +0530

    second commit

--- app/src/main/java/com/example/gamef/testproject/MainActivity.java
+++ app/src/main/java/com/example/gamef/testproject/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int i=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        i=1;
     }
 }
