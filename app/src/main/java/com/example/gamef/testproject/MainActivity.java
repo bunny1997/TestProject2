8dfb6cc7475189b6db384f3466b48b0c62afa603
bunny1997 <priyank20patel@gmail.com>
2017-02-10 15:38:47 +0530

    second commit

--- app/src/main/java/com/example/gamef/testproject/MainActivity.java
+++ app/src/main/java/com/example/gamef/testproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gamef.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         i=1;
+        Toast.makeText(this, "from priyank", Toast.LENGTH_SHORT).show();
     }
 }
